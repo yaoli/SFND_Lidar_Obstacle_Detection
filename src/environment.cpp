45fa31a72c6636a990a53cbd68016e9803b9d3e1
Li Yao <yaoli.email@gmail.com>
2020-09-12 21:43:08 -0700

    better

--- src/environment.cpp
+++ src/environment.cpp
@@ -8,21 +8,21 @@
 // using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
 
-std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
+std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
 
-    Car egoCar( Vect3(0,0,0), Vect3(4,2,2), Color(0,1,0), "egoCar");
-    Car car1( Vect3(15,0,0), Vect3(4,2,2), Color(0,0,1), "car1");
-    Car car2( Vect3(8,-4,0), Vect3(4,2,2), Color(0,0,1), "car2");	
-    Car car3( Vect3(-12,4,0), Vect3(4,2,2), Color(0,0,1), "car3");
-  
+    Car egoCar(Vect3(0, 0, 0), Vect3(4, 2, 2), Color(0, 1, 0), "egoCar");
+    Car car1(Vect3(15, 0, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car1");
+    Car car2(Vect3(8, -4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car2");
+    Car car3(Vect3(-12, 4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car3");
+
     std::vector<Car> cars;
     cars.push_back(egoCar);
     cars.push_back(car1);
     cars.push_back(car2);
     cars.push_back(car3);
 
-    if(renderScene)
+    if (renderScene)
     {
         renderHighway(viewer);
         egoCar.render(viewer);
@@ -34,66 +34,89 @@ std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer
     return cars;
 }
 
-
-void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
+void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
     // ----------------------------------------------------
     // -----Open 3D viewer and display simple highway -----
     // ----------------------------------------------------
-    
+
     // RENDER OPTIONS
     bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
-    
-    // TODO:: Create lidar sensor on the heap, not on the stack (way too small)
-    Lidar* lidar = new Lidar(cars, 0.);
+
+    // Create lidar sensor on the heap, not on the stack (way too small)
+    Lidar *lidar = new Lidar(cars, 0.);
     pcl::PointCloud<pcl::PointXYZ>::Ptr points = lidar->scan();
     //renderRays(viewer, lidar->position, points);
     renderPointCloud(viewer, points, "point cloud", Color(255., 255., 255.));
-    // TODO:: Create point processor
-    ProcessPointClouds<pcl::PointXYZ> processor;
-    std::pair<typename pcl::PointCloud<pcl::PointXYZ>::Ptr, typename pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = processor.Segment(points, 100, 0.2);
-    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
-    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
-    
+
+    // Create point processor
+    ProcessPointClouds<pcl::PointXYZ> pp;
+
+    // segmentation
+    int max_iter = 200;
+    float distance_tol = 0.2; // within this distance considered as belonging to inliers
+    std::pair<typename pcl::PointCloud<pcl::PointXYZ>::Ptr, typename pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pp.Segment(points, max_iter, distance_tol);
+    //renderPointCloud(viewer, segmentCloud.first, "road", Color(1, 0, 0));
+    //renderPointCloud(viewer, segmentCloud.second, "obstacle", Color(0, 1, 0));
+
+    // clustering
+    float radius = 1.0;
+    int min_n_points = 3;
+    int max_n_points = 30;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pp.Clustering(segmentCloud.second, radius, min_n_points, max_n_points);
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)}; // this is called "list initialization", specific to STL
+    for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    {
+        std::cout << "cluster size ";
+        pp.numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obstacle" + std::to_string(clusterId), colors[clusterId]);
+        ++clusterId;
+    }
 }
 
-
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
-void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer)
+void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
 
-    viewer->setBackgroundColor (0, 0, 0);
-    
+    viewer->setBackgroundColor(0, 0, 0);
+
     // set camera position and angle
     viewer->initCameraParameters();
     // distance away in meters
     int distance = 16;
-    
-    switch(setAngle)
+
+    switch (setAngle)
     {
-        case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
-        case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
-        case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
-        case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
+    case XY:
+        viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0);
+        break;
+    case TopDown:
+        viewer->setCameraPosition(0, 0, distance, 1, 0, 1);
+        break;
+    case Side:
+        viewer->setCameraPosition(0, -distance, 0, 0, 0, 1);
+        break;
+    case FPS:
+        viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
     }
 
-    if(setAngle!=FPS)
-        viewer->addCoordinateSystem (1.0);
+    if (setAngle != FPS)
+        viewer->addCoordinateSystem(1.0);
 }
 
-
-int main (int argc, char** argv)
+int main(int argc, char **argv)
 {
     std::cout << "starting enviroment" << std::endl;
 
-    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
     simpleHighway(viewer);
 
-    while (!viewer->wasStopped ())
+    while (!viewer->wasStopped())
     {
-        viewer->spinOnce ();
-    } 
+        viewer->spinOnce();
+    }
 }
--- src/processPointClouds.cpp
+++ src/processPointClouds.cpp
@@ -38,7 +38,8 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     // TODO: Create two new point clouds, one cloud with obstacles and other with segmented plane
     typename pcl::PointCloud<PointT>::Ptr obstCloud(new pcl::PointCloud<PointT>());
     typename pcl::PointCloud<PointT>::Ptr planeCloud(new pcl::PointCloud<PointT>());
-    for(int index : inliers->indices){
+    for (int index : inliers->indices)
+    {
         planeCloud->points.push_back(cloud->points[index]);
     }
     pcl::ExtractIndices<PointT> extract;
@@ -57,65 +58,72 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     auto startTime = std::chrono::steady_clock::now();
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
     std::unordered_set<int> inliersResult;
-	srand(time(NULL));
-	while (maxIterations--) {
-		std::unordered_set<int> inliers;
-		while (inliers.size() < 3) {
-			inliers.insert(rand() % (cloud -> points.size()));
-		}
-		auto itr = inliers.begin();
-		float x1 = cloud->points[*itr].x;
-		float y1 = cloud->points[*itr].y;
-		float z1 = cloud->points[*itr].z;
-		itr++;
-		float x2 = cloud->points[*itr].x;
-		float y2 = cloud->points[*itr].y;
-		float z2 = cloud->points[*itr].z;
-		itr++;
-		float x3 = cloud->points[*itr].x;
-		float y3 = cloud->points[*itr].y;
-		float z3 = cloud->points[*itr].z;
-		// the coefficient of the plane with three points
-		float a = (y2-y1) * (z3 - z1) - (z2-z1)*(y3-y1);
-		float b = (z2-z1)*(x3-x1)-(x2-x1)*(z3-z1);
-		float c = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1);
-		float d = -(a * x1 + b * y1 + c * z1);
-		// compute distance
-		for (int index = 0; index < cloud->points.size(); index++) {
-			if (inliers.count(index) > 0) {
-				// match two points that parameterize the line
-				continue;
-			}
-			pcl::PointXYZ point = cloud->points[index];
-			float x4 = point.x;
-			float y4 = point.y;
-			float z4 = point.z;
-			float d = fabs(a*x4 + b*y4 + c*z4 + d) / sqrt(a*a + b*b + c*c + d*d);
-			if (d < distanceThreshold) {
-				inliers.insert(index);
-			}
-		}
-		if (inliers.size() > inliersResult.size()) {
-			inliersResult = inliers;
-		}
-	}
-    if (inliersResult.size() == 0) {
+    srand(time(NULL));
+    while (maxIterations--)
+    {
+        std::unordered_set<int> inliers;
+        while (inliers.size() < 3)
+        {
+            inliers.insert(rand() % (cloud->points.size()));
+        }
+        auto itr = inliers.begin();
+        float x1 = cloud->points[*itr].x;
+        float y1 = cloud->points[*itr].y;
+        float z1 = cloud->points[*itr].z;
+        itr++;
+        float x2 = cloud->points[*itr].x;
+        float y2 = cloud->points[*itr].y;
+        float z2 = cloud->points[*itr].z;
+        itr++;
+        float x3 = cloud->points[*itr].x;
+        float y3 = cloud->points[*itr].y;
+        float z3 = cloud->points[*itr].z;
+        // the coefficient of the plane with three points
+        float a = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+        float b = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+        float c = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+        float d = -(a * x1 + b * y1 + c * z1);
+        // compute distance
+        for (int index = 0; index < cloud->points.size(); index++)
+        {
+            if (inliers.count(index) > 0)
+            {
+                // match two points that parameterize the line
+                continue;
+            }
+            pcl::PointXYZ point = cloud->points[index];
+            float x4 = point.x;
+            float y4 = point.y;
+            float z4 = point.z;
+            float d = fabs(a * x4 + b * y4 + c * z4 + d) / sqrt(a * a + b * b + c * c + d * d);
+            if (d < distanceThreshold)
+            {
+                inliers.insert(index);
+            }
+        }
+        if (inliers.size() > inliersResult.size())
+        {
+            inliersResult = inliers;
+        }
+    }
+    if (inliersResult.size() == 0)
+    {
         std::cout << "Could not estimate a planar model for the given dataset!" << std::endl;
     }
 
     // build inliers and outliers
-    pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
 
-	for(int index = 0; index < cloud->points.size(); index++)
-	{
-		pcl::PointXYZ point = cloud->points[index];
-		if(inliersResult.count(index))
-			cloudInliers->points.push_back(point);
-		else
-			cloudOutliers->points.push_back(point);
-	}
-    
+    for (int index = 0; index < cloud->points.size(); index++)
+    {
+        pcl::PointXYZ point = cloud->points[index];
+        if (inliersResult.count(index))
+            cloudInliers->points.push_back(point);
+        else
+            cloudOutliers->points.push_back(point);
+    }
+
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "plane segmentation took " << elapsedTime.count() << " milliseconds" << std::endl;
@@ -140,7 +148,8 @@ std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT
     seg.setMaxIterations(maxIterations);
     seg.setInputCloud(cloud);
     seg.segment(*inliers, *coefficients);
-    if (inliers->indices.size() == 0) {
+    if (inliers->indices.size() == 0)
+    {
         std::cout << "Could not estimate a planar model for the given dataset!" << std::endl;
     }
     auto endTime = std::chrono::steady_clock::now();
@@ -158,10 +167,28 @@ std::vector<typename pcl::PointCloud<PointT>::Ptr> ProcessPointClouds<PointT>::C
     // Time clustering process
     auto startTime = std::chrono::steady_clock::now();
 
+    // perform euclidean clustering to group detected obstacles
     std::vector<typename pcl::PointCloud<PointT>::Ptr> clusters;
-
-    // TODO:: Fill in the function to perform euclidean clustering to group detected obstacles
-
+    typename pcl::search::KdTree<PointT>::Ptr tree(new pcl::search::KdTree<PointT>);
+    tree->setInputCloud(cloud);
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<PointT> ec;
+    ec.setClusterTolerance(clusterTolerance);
+    ec.setMinClusterSize(minSize);
+    ec.setMaxClusterSize(maxSize);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud);
+    ec.extract(cluster_indices);
+    for (pcl::PointIndices getIndices : cluster_indices)
+    {
+        typename pcl::PointCloud<PointT>::Ptr cluster(new pcl::PointCloud<PointT>);
+        for (int index : getIndices.indices)
+            cluster->points.push_back(cloud->points[index]);
+        cluster->width = cluster->points.size();
+        cluster->height = 1;
+        clusters.push_back(cluster);
+    }
+    
     auto endTime = std::chrono::steady_clock::now();
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "clustering took " << elapsedTime.count() << " milliseconds and found " << clusters.size() << " clusters" << std::endl;
